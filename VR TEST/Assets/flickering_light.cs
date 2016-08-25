631fe0796d12424d0a74ef0c0c88369d35737486
Travis Cornelius <traviscornelius@Traviss-MacBook-Pro.local>
2016-08-25 15:47:10 -0500

    up

---
+++ VR TEST/Assets/flickering_light.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flickering_light : MonoBehaviour {
+
+	public float timer = 5;
+	private Light[] lights;
+	public GameObject lightParent;
+
+	void Update () { 
+
+		timer -= Time.deltaTime;
+
+		if(timer < 0){
+			lights = GetComponentsInChildren<Light>(true);
+
+			foreach (Light light in lights)
+			{
+				if (GetComponent<Light>().enabled == true){
+					GetComponent<Light>().enabled = false; 
+				} else if (GetComponent<Light>().enabled == false){
+					Debug.Log("I'm flashing you."); 
+					GetComponent<Light>().enabled = true; 
+				} 
+				timer = 5;
+			}
+
+		}
+	}
+
+}
