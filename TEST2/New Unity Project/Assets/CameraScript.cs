b49287dfa1b657eb3970774f765a8d329bce185b
Travis Cornelius <traviscornelius@Traviss-MacBook-Pro.local>
2016-08-25 15:39:34 -0500

    test portal

---
+++ TEST2/New Unity Project/Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("start");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		
+		if(col.gameObject.name == "ZombiePortal")
+		{
+			Debug.Log ("Collision");
+
+			Application.LoadLevel ("Zombie");
+		}
+	}
+}
