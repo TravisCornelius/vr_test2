c99d597153ff02da65ba2b083c4c913544f019bb
Travis Cornelius <traviscornelius@Traviss-MacBook-Pro.local>
2016-09-02 14:20:54 -0500

    updated

--- TEST2/New Unity Project/Assets/Lighting.cs
+++ TEST2/New Unity Project/Assets/Lighting.cs
@@ -9,13 +9,18 @@ public class Lighting : MonoBehaviour {
 	public AudioClip roboticWave;
 	bool triggeredLightEffect = false;
 	int numOfBlinks = 5;
+	public GameObject ghostRadio;
+	public GameObject normalRadio;
+	AudioSource ghostSource;
+	AudioSource normalSource;
 
 	AudioSource audioSource;
 
 	void Start() {
 		lt = GetComponent<Light>();
 		audioSource = GetComponent<AudioSource>();
-
+		ghostSource = ghostRadio.GetComponent<AudioSource> ();
+		normalSource = normalRadio.GetComponent<AudioSource> ();
 	}
 	void Update() {
 		if (!triggeredLightEffect && PlayerScript.solvedPuzzle1) {
@@ -34,8 +39,11 @@ public class Lighting : MonoBehaviour {
 
 				audioSource.volume = amplitude;
 			} else {
+				
 				triggeredLightEffect = true;
 				StartCoroutine(lightGhostWorld ());
+
+
 			}
 		}
 	}
@@ -44,13 +52,18 @@ public class Lighting : MonoBehaviour {
 	{
 		Debug.Log ("LIGHT THE GHOST WORLD");
 		audioSource.Stop ();
-		PlayerScript.ghostMode = true;
+
 		lt.intensity = 0;
 		audioSource.volume = 1;
 		audioSource.clip = roboticWave;
 		audioSource.Play ();
-		yield return new WaitForSeconds( audioSource.clip.length );
-		lt.intensity = 1;
+		//yield return new WaitForSeconds( audioSource.clip.length );
 
+		yield return new WaitForSeconds( 2 );
+		normalSource.Stop ();
+		ghostSource.Play ();
+		PlayerScript.ghostMode = true;
+		lt.intensity = 1;
+	
 	}
 }
---
+++ TEST2/New Unity Project/Assets/SkinnedGhostMode.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkinnedGhostMode : MonoBehaviour {
+
+	Shader originalShader;
+	Shader ghostShader;
+
+	SkinnedMeshRenderer rend;
+	public bool dontRender = false;
+	Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rend = GetComponent<SkinnedMeshRenderer>();        
+		originalShader = rend.material.shader;
+		ghostShader = Shader.Find ("Ciconia Studio/Effects/Ghost/Old version(1.2)/Ghost Animated Details");
+		DisablePhysics ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!PlayerScript.ghostMode) {
+			if (dontRender)
+			{
+				rend.enabled = false;
+			} else
+			{
+				rend.material.shader = ghostShader;
+				rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+			}
+			DisablePhysics ();
+		} else {
+			rend.enabled = true;
+			rend.material.shader = originalShader;
+			rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+			EnablePhysics ();
+		}
+	}
+	void EnablePhysics() {
+		rb.isKinematic = false;
+		rb.detectCollisions = true;
+	}
+	void DisablePhysics() {
+		rb.isKinematic = true;
+		rb.detectCollisions = false;
+	}
+}
